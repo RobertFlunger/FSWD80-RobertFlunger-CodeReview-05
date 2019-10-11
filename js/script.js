6f1300efd100b7992653a48193f7feae760e9352
RobertFlunger <robert.flunger@gmx.at>
2019-10-12 00:36:45 +0200

    further tweaks

--- js/script.js
+++ js/script.js
@@ -27,15 +27,21 @@ function addMovie (img, name, description) {
 	$(movie).appendTo(".movie-wrapper");				
 }
 
-for (let i =0; i<movies.length; i++) {
-	addMovie(movies[i].img, movies[i].name, movies[i].description);
-}
 
+function createList() {
+	for (let i =0; i<movies.length; i++) {
+	addMovie(movies[i].img, movies[i].name, movies[i].description);
+	}
+	standard = $(".movie-description");
+}
+createList()
+
+/*
 for (let i = 0; i<6; i++) {
 	$("#title" + i).text(movies[i].name);
 	$("#desc" + i).text(movies[i].description);
 	$("#img-cont" + i).html("<img class='img-fluid h-100 w-100 rounded border' src='" + movies[i].img + "'>");
-}
+}*/
 
 $(".like-btn").on('click', function() {
 		let y = $(this).parent().find(".like-amnt").text();
@@ -44,23 +50,24 @@ $(".like-btn").on('click', function() {
 		$(this).parent().find(".like-amnt").text(y);
 	});
 
-var standard = $(".movie-description");
-
 $("#default-order").on('click', function() {
 	$(".movie-wrapper").append(standard);
 });
 
 $("#descending-sort").on('click', function() {
-	var ordered = $(".movie-description").sort(function (a,b) {
-		return $(a).find(".like-amnt").text() < $(b).find(".like-amnt").text() ? 1 : -1;
+	ordered = $(".movie-description").sort(function (a,b) {
+		return parseInt($(a).find(".like-amnt").text()) < parseInt($(b).find(".like-amnt").text()) ? 1 : -1;
+	});
+	$(".movie-wrapper").append(ordered);
+	})
+	/*$(ordered).appendTo(".movie-wrapper");*/
+
+$("#ascending-sort").on('click', function() {
+	var ordered = "";
+	ordered = $(".movie-description").sort(function (a,b) {
+		return parseInt($(a).find(".like-amnt").text()) > parseInt($(b).find(".like-amnt").text()) ? 1 : -1;
 	});
 	$(".movie-wrapper").append(ordered);
 });
 
-$("#ascending-sort").on('click', function() {
-	var ordered = $(".movie-description").sort(function (a,b) {
-		return $(a).find(".like-amnt").text() > $(b).find(".like-amnt").text() ? 1 : -1;
-	});
-	$(".movie-wrapper").append(ordered);
-});
 
