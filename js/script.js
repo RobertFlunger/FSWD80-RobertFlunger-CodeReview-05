6e020d27db5c6c24181e613360b3da2804170e82
RobertFlunger <robert.flunger@gmx.at>
2019-10-11 21:13:44 +0200

    js + refinements

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ console.log(movies[0].name)
 for (let i = 0; i<6; i++) {
 	$("#title" + i).text(movies[i].name);
 	$("#desc" + i).text(movies[i].description);
-	$("#img-cont" + i).html("<img class='img-fluid h-100 w-100' src='" + movies[i].img + "'>");
+	$("#img-cont" + i).html("<img class='img-fluid h-100 w-100 rounded border' src='" + movies[i].img + "'>");
 }
 
 $(".like-btn").on('click', function() {
@@ -16,21 +16,23 @@ $(".like-btn").on('click', function() {
 		$(this).parent().find(".like-amnt").text(y);
 	});
 
-console.log(movies.length)
-var length = 7
-var sorted = new Array();
-$("#order").on('click', function() {
-	console.log($(".movie-description:nth-child(" + 1 + ") .like-amnt").text());
-	for (let i = 1; i<length; i++) {
-		sorted[i] = $(".movie-description:nth-child(" + i + ") .like-amnt").attr('id');
-		
-	}
-	
-	sorted = sorted.sort().reverse();
-	console.log(sorted);
-/*
-	$("#likes0").closest(".movie-description").addClass("order-0");
-	$("#likes2").closest(".movie-description").addClass("order-1");
-*/
+var standard = $(".movie-description");
 
-})
+$("#default-order").on('click', function() {
+	$(".movie-wrapper").append(standard);
+});
+
+$("#descending-sort").on('click', function() {
+	var ordered = $(".movie-description").sort(function (a,b) {
+		return $(a).find(".like-amnt").text() < $(b).find(".like-amnt").text() ? 1 : -1;
+	});
+	$(".movie-wrapper").append(ordered);
+});
+
+$("#ascending-sort").on('click', function() {
+	var ordered = $(".movie-description").sort(function (a,b) {
+		return $(a).find(".like-amnt").text() > $(b).find(".like-amnt").text() ? 1 : -1;
+	});
+	$(".movie-wrapper").append(ordered);
+});
+
